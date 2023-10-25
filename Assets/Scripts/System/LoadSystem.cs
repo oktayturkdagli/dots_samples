f74df11d5759996b29081ecfb89a8ecfa1a89ea7
Oktay Türkdağlı <oktayturkdagli@gmail.com>
2023-10-25 15:19:36 +0300

    refactor(system): separate functions in load system

--- Assets/Scripts/System/LoadSystem.cs
+++ Assets/Scripts/System/LoadSystem.cs
@@ -27,17 +27,33 @@ namespace System
         [BurstCompile]
         public void OnUpdate(ref SystemState state)
         {
-            var ecbForNode = SystemAPI.GetSingleton<EndInitializationEntityCommandBufferSystem.Singleton>()
-                .CreateCommandBuffer(state.WorldUnmanaged).AsParallelWriter();
-            var ecbForWay = SystemAPI.GetSingleton<EndInitializationEntityCommandBufferSystem.Singleton>()
-                .CreateCommandBuffer(state.WorldUnmanaged).AsParallelWriter();
-            var ecbForLanelet = SystemAPI.GetSingleton<EndInitializationEntityCommandBufferSystem.Singleton>()
-                .CreateCommandBuffer(state.WorldUnmanaged).AsParallelWriter();
-            
+            var prefabComponent = SystemAPI.GetSingleton<PrefabComponent>();
             var loadComponent = SystemAPI.GetSingleton<LoadComponent>();
             var loadComponentEntity = SystemAPI.GetSingletonEntity<LoadComponent>();
-            var prefabComponent = SystemAPI.GetSingleton<PrefabComponent>();
 
+            ScheduleJobs(ref state, loadComponent, prefabComponent);
+            DestroyEntity(ref state, loadComponentEntity);
+        }
+
+        [BurstCompile]
+        private void ScheduleJobs(ref SystemState state, LoadComponent loadComponent, PrefabComponent prefabComponent)
+        {
+            var nodeInstantiateParallelJobHandle = ScheduleNodeJob(ref state, loadComponent, prefabComponent);
+            var wayInstantiateParallelJobHandle = ScheduleWayJob(ref state, loadComponent);
+            var laneletInstantiateParallelJobHandle = ScheduleLaneletJob(ref state, loadComponent, prefabComponent);
+            
+            state.Dependency = JobHandle.CombineDependencies(
+                nodeInstantiateParallelJobHandle, 
+                wayInstantiateParallelJobHandle, 
+                laneletInstantiateParallelJobHandle);
+        }
+        
+        [BurstCompile]
+        private JobHandle ScheduleNodeJob(ref SystemState state, LoadComponent loadComponent, PrefabComponent prefabComponent)
+        {
+            var ecbForNode = SystemAPI.GetSingleton<EndInitializationEntityCommandBufferSystem.Singleton>()
+                .CreateCommandBuffer(state.WorldUnmanaged).AsParallelWriter();
+            
             var nodeInstantiateParallelJobHandle = new NodeInstantiateParallelJob
             {
                 SortKey = 0,
@@ -47,6 +63,15 @@ namespace System
                 NodeScale = prefabComponent.NodeScale
             }.Schedule(loadComponent.NodeDataNativeArray.Length, loadComponent.NodeDataNativeArray.Length / 4, state.Dependency);
             
+            return nodeInstantiateParallelJobHandle;
+        }
+        
+        [BurstCompile]
+        private JobHandle ScheduleWayJob(ref SystemState state, LoadComponent loadComponent)
+        {
+            var ecbForWay = SystemAPI.GetSingleton<EndInitializationEntityCommandBufferSystem.Singleton>()
+                .CreateCommandBuffer(state.WorldUnmanaged).AsParallelWriter();
+            
             var wayInstantiateParallelJobHandle = new WayInstantiateParallelJob
             {
                 SortKey = 0,
@@ -55,6 +80,15 @@ namespace System
                 NodeDataNativeArray = loadComponent.NodeDataNativeArray,
                 NodeListForWayNativeList = loadComponent.NodeListForWayNativeList
             }.Schedule(loadComponent.WayDataNativeArray.Length, loadComponent.WayDataNativeArray.Length / 4, state.Dependency);
+            
+            return wayInstantiateParallelJobHandle;
+        }
+        
+        [BurstCompile]
+        private JobHandle ScheduleLaneletJob(ref SystemState state, LoadComponent loadComponent, PrefabComponent prefabComponent)
+        {
+            var ecbForLanelet = SystemAPI.GetSingleton<EndInitializationEntityCommandBufferSystem.Singleton>()
+                .CreateCommandBuffer(state.WorldUnmanaged).AsParallelWriter();
             
             var laneletInstantiateParallelJobHandle = new LaneletInstantiateParallelJob
             {
@@ -67,14 +101,15 @@ namespace System
                 NodeListForWayNativeList = loadComponent.NodeListForWayNativeList
             }.Schedule(loadComponent.LaneletDataNativeArray.Length, loadComponent.LaneletDataNativeArray.Length / 4, state.Dependency);
             
-            state.Dependency = JobHandle.CombineDependencies(
-                nodeInstantiateParallelJobHandle, 
-                wayInstantiateParallelJobHandle, 
-                laneletInstantiateParallelJobHandle);
-            
+            return laneletInstantiateParallelJobHandle;
+        }
+
+        [BurstCompile]
+        private void DestroyEntity(ref SystemState state, Entity entity)
+        {
             SystemAPI.GetSingleton<BeginPresentationEntityCommandBufferSystem.Singleton>()
                 .CreateCommandBuffer(state.WorldUnmanaged)
-                .DestroyEntity(loadComponentEntity);
+                .DestroyEntity(entity);
         }
     }
 }
