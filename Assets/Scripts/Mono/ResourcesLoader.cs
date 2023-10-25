733b70fa07f38b0e55e8b9502edb74ebfcc16b4e
Oktay Türkdağlı <oktayturkdagli@gmail.com>
2023-10-25 09:51:20 +0300

    feat(util): add resource loader class

---
+++ Assets/Scripts/Mono/ResourcesLoader.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Mono
+{
+    public class ResourcesLoader : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Resources.LoadAsync("Prefabs/Line");
+            Resources.LoadAsync("Materials/Mat_Lanelet");
+            Resources.LoadAsync("Materials/Mat_Line_Arrow_Bidirectional_Dashed");
+            Resources.LoadAsync("Materials/Mat_Line_Arrow_Bidirectional_Solid");
+            Resources.LoadAsync("Materials/Mat_Line_Arrow_Common_Dashed");
+            Resources.LoadAsync("Materials/Mat_Line_Arrow_Common_Solid");
+        }
+    }
+}
