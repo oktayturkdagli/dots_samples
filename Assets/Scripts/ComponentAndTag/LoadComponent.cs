06343b5ba20ce5fed4c1da7df0ebacd93d99a924
Oktay Türkdağlı <oktayturkdagli@gmail.com>
2023-10-25 09:50:21 +0300

    feat(component): add load component

---
+++ Assets/Scripts/ComponentAndTag/LoadComponent.cs
@@ -0,0 +1,14 @@
+﻿using Data;
+using Unity.Collections;
+using Unity.Entities;
+
+namespace ComponentAndTag
+{
+    public struct LoadComponent : IComponentData
+    {
+        public NativeArray<NodeData> NodeData;
+        public NativeArray<WayData> WayData;
+        public NativeArray<LaneletData> LaneletData;
+        public NativeList<int> NodeListForWay;
+    }
+}
