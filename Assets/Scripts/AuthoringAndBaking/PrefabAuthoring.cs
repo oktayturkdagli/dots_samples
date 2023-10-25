d8a9deaa160257db519100b08e97537d9319e7d5
Oktay Türkdağlı <oktayturkdagli@gmail.com>
2023-10-25 10:35:24 +0300

    feat(authoring): add prefab authoring

---
+++ Assets/Scripts/AuthoringAndBaking/PrefabAuthoring.cs
@@ -0,0 +1,39 @@
+﻿using ComponentAndTag;
+using Unity.Entities;
+using UnityEngine;
+
+namespace AuthoringAndBaking
+{
+    public class PrefabAuthoring : MonoBehaviour
+    {
+        public GameObject nodePrefab;
+        public GameObject laneletPrefab;
+        public float nodeScale;
+    }
+    
+    public class PrefabBaker : Baker<PrefabAuthoring>
+    {
+        public override void Bake(PrefabAuthoring authoring)
+        {
+            DependsOn(authoring.nodePrefab);
+            DependsOn(authoring.nodePrefab);
+            
+            if (authoring.nodePrefab == null)
+            {
+                Debug.Log("Prefab References Are NULL");
+                return;
+            }
+            
+            var nodePrefabEntity = GetEntity(authoring.nodePrefab, TransformUsageFlags.Dynamic);
+            var laneletPrefabEntity = GetEntity(authoring.laneletPrefab, TransformUsageFlags.Renderable);
+            var prefabPropertyEntity = GetEntity(TransformUsageFlags.None);
+            
+            AddComponent(prefabPropertyEntity, new PrefabComponent
+            {
+                NodePrefabEntity = nodePrefabEntity,
+                LaneletPrefabEntity = laneletPrefabEntity,
+                NodeScale = authoring.nodeScale
+            });
+        }
+    }
+}
