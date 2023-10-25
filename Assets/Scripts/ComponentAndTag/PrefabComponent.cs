cfba031e7ab12cf4c6795189d61252ccba33d02c
Oktay Türkdağlı <oktayturkdagli@gmail.com>
2023-10-25 10:34:58 +0300

    feat(component): add prefab component

---
+++ Assets/Scripts/ComponentAndTag/PrefabComponent.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Entities;
+
+namespace ComponentAndTag
+{
+    public struct PrefabComponent : IComponentData
+    {
+        public Entity NodePrefabEntity;
+        public Entity LaneletPrefabEntity;
+        public float NodeScale;
+    }
+}
