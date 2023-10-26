1ab817417b40879d9fc343393a14510b99c4d990
Oktay Türkdağlı <oktayturkdagli@gmail.com>
2023-10-26 13:54:46 +0300

    feat(system): add mesh load system

---
+++ Assets/Scripts/Extension/MeshExtension.cs
@@ -0,0 +1,180 @@
+﻿using Unity.Collections;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Extension
+{
+    public static class MeshExtension
+    {
+        public static Mesh BuildMeshForLanelet(NativeList<float3> leftVertices, NativeList<float3> rightVertices)
+        {
+            //Find bigger way
+            var isEqual = leftVertices.Length == rightVertices.Length;
+
+            // Combine in one array
+            // Line left is assigned to even numbers
+            // Line right is assigned to odd numbers
+            Vector3[] vertices;
+            var smallCounter = -1;
+            if (isEqual)
+            {
+                vertices = new Vector3[leftVertices.Length * 2];
+                for (var i = 0; i < leftVertices.Length; i++)
+                    vertices[i * 2] = leftVertices[i];
+
+                foreach (var item in rightVertices)
+                {
+                    smallCounter += 2;
+                    vertices[smallCounter] = item;
+                }
+            }
+            else
+            {
+                NativeList<float3> bigger;
+                NativeList<float3> smaller;
+
+                if (leftVertices.Length >= rightVertices.Length)
+                {
+                    bigger = leftVertices;
+                    smaller = rightVertices;
+                }
+                else
+                {
+                    bigger = rightVertices;
+                    smaller = leftVertices;
+                }
+
+                vertices = new Vector3[bigger.Length * 2];
+                for (var i = 0; i < bigger.Length; i++)
+                    vertices[i * 2] = bigger[i];
+
+                foreach (var item in smaller)
+                {
+                    smallCounter += 2;
+                    vertices[smallCounter] = item;
+                }
+                    
+            }
+
+            var triangles = CalculateTrianglesForLanelet(leftVertices.Length + rightVertices.Length, isEqual, vertices);
+            return CreateMesh(vertices, triangles);
+        }
+
+        private static Mesh CreateMesh(Vector3[] vertices, int[] triangles)
+        {
+            Vector2[] uvs = new Vector2[vertices.Length];
+            for (int i = 0; i < uvs.Length; i++)
+            {
+                uvs[i] = new Vector2(vertices[i].x, vertices[i].z);
+            }
+
+            var mesh = new Mesh
+            {
+                vertices = vertices,
+                triangles = triangles,
+                uv = uvs
+            };
+
+            mesh.RecalculateNormals();
+            return mesh;
+        }
+
+        private static int[] CalculateTrianglesForLanelet(int sumOfVerticesCount, bool isEqual, Vector3[] vertices)
+        {
+            // Find triangles count
+            var triangles = new int[(sumOfVerticesCount - 2) * 3];
+            var isUsingEqualityAlgorithm = true;
+            var isTurnLeft = false;
+
+            var firstVertex = 0; // A[0]
+            var secondVertex = 2; // A[1]
+            var thirdVertex = 1; // B[0]
+
+            // Detect triangles
+            if (isEqual)
+            {
+                // First triangle
+                triangles[0] = firstVertex;
+                triangles[1] = secondVertex;
+                triangles[2] = thirdVertex;
+
+                // Second and others
+                for (var i = 3; i < triangles.Length; i += 3)
+                {
+                    firstVertex += 1;
+                    if (i % 2 == 0)
+                        secondVertex += 2;
+                    else
+                        thirdVertex += 2;
+
+                    triangles[i] = firstVertex;
+                    triangles[i + 1] = secondVertex;
+                    triangles[i + 2] = thirdVertex;
+                }
+            }
+            else
+            {
+                // First triangle
+                triangles[0] = firstVertex;
+                triangles[1] = secondVertex;
+                triangles[2] = thirdVertex;
+
+                // Second and others
+                for (var i = 3; i < triangles.Length; i += 3)
+                {
+                    if (isUsingEqualityAlgorithm)
+                    {
+                        isTurnLeft = !isTurnLeft;
+
+                        // First algorithm (same with equality algorithm)
+                        firstVertex += 1;
+                        if (i % 2 == 0)
+                            secondVertex += 2;
+                        else
+                            thirdVertex += 2;
+                    }
+
+                    // If there is no a node change the algorithm 
+                    if (isUsingEqualityAlgorithm && vertices[firstVertex] == Vector3.zero ||
+                        vertices[secondVertex] == Vector3.zero || vertices[thirdVertex] == Vector3.zero)
+                    {
+                        // Reset
+                        if (i % 2 == 0)
+                            secondVertex -= 2;
+                        else
+                            thirdVertex -= 2;
+
+                        // Assign default values
+                        if (isTurnLeft)
+                        {
+                            firstVertex = thirdVertex;
+                            thirdVertex = secondVertex;
+                            secondVertex -= 2;
+                        }
+                        else
+                        {
+                            firstVertex = secondVertex;
+                            secondVertex = thirdVertex;
+                            thirdVertex = thirdVertex - 2;
+                        }
+
+                        isUsingEqualityAlgorithm = false;
+                    }
+
+                    if (!isUsingEqualityAlgorithm)
+                    {
+                        // Second Algorithm
+                        secondVertex += 2;
+                        thirdVertex += 2;
+                    }
+
+                    triangles[i] = firstVertex;
+                    triangles[i + 1] = secondVertex;
+                    triangles[i + 2] = thirdVertex;
+                }
+            }
+
+            return triangles;
+        }
+    }
+}
---
+++ Assets/Scripts/Systems/MeshLoadSystem.cs
@@ -0,0 +1,69 @@
+﻿using ComponentAndTag;
+using Extension;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Rendering;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace Systems
+{
+    [UpdateInGroup(typeof(SimulationSystemGroup), OrderFirst = true)]
+    [BurstCompile]
+    public partial struct MeshLoadSystem : ISystem
+    {
+        [BurstCompile]
+        public void OnCreate(ref SystemState state)
+        {
+            state.RequireForUpdate<LoadComponent>();
+        }
+
+        [BurstCompile]
+        public void OnDestroy(ref SystemState state)
+        {
+        }
+    
+        public void OnUpdate(ref SystemState state)
+        {
+            var laneletMaterial = Resources.Load<Material>($"Materials/MAT_Lanelet");
+            var desc = new RenderMeshDescription(ShadowCastingMode.Off, false, MotionVectorGenerationMode.ForceNoMotion);
+            var materialMeshInfo = MaterialMeshInfo.FromRenderMeshArrayIndices(0, 0);
+            var query = SystemAPI.QueryBuilder().WithAll<LaneletComponent>().Build();
+            var entities = query.ToEntityArray(Allocator.Temp);
+            var loadComponent = SystemAPI.GetSingleton<LoadComponent>();
+        
+            var firstLaneletID = loadComponent.LaneletDataNativeArray[0].ID;
+            var firstWayID = loadComponent.WayDataNativeArray[0].ID;
+            var leftVerticesNativeList = new NativeList<float3>(64,Allocator.TempJob);
+            var rightVerticesNativeList = new NativeList<float3>(64,Allocator.TempJob);
+
+            foreach (var entity in entities)
+            {
+                leftVerticesNativeList.Clear();
+                rightVerticesNativeList.Clear();
+            
+                var laneletComponent = state.EntityManager.GetComponentData<LaneletComponent>(entity);
+                var leftWay = loadComponent.WayDataNativeArray[loadComponent.LaneletDataNativeArray[laneletComponent.ID - firstLaneletID].LeftWayId - firstWayID];
+                var rightWay = loadComponent.WayDataNativeArray[loadComponent.LaneletDataNativeArray[laneletComponent.ID - firstLaneletID].RightWayId - firstWayID];
+            
+                for (var i = 0; i < leftWay.NodeCount; i++)
+                    leftVerticesNativeList.Add(
+                        loadComponent.NodeDataNativeArray[loadComponent.NodeListForWayNativeList[leftWay.SliceStartId + i] - 1].Position
+                    );
+            
+                for (var i = 0; i < rightWay.NodeCount; i++)
+                    rightVerticesNativeList.Add(
+                        loadComponent.NodeDataNativeArray[loadComponent.NodeListForWayNativeList[rightWay.SliceStartId + i] - 1].Position
+                    );
+            
+                var mesh = MeshExtension.BuildMeshForLanelet(leftVerticesNativeList, rightVerticesNativeList);
+                if (laneletMaterial == null || mesh == null)
+                    return;
+                var meshArray = new RenderMeshArray(new[] { laneletMaterial }, new[] { mesh });
+                RenderMeshUtility.AddComponents(entity, state.EntityManager, desc, meshArray, materialMeshInfo);
+            }
+        }
+    }
+}
