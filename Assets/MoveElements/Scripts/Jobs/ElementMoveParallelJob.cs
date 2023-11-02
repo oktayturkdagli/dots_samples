bb13b626e89cdf0549ebd899d16573c552b62b2f
Oktay Türkdağlı <oktayturkdagli@gmail.com>
2023-11-02 13:29:52 +0300

    feat(movement): add movement module

---
+++ Assets/MoveElements/Scripts/Jobs/ElementMoveParallelJob.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.InteropServices;
+using MoveElements.Scripts.Components;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+namespace MoveElements.Scripts.Jobs
+{
+    [BurstCompile]
+    [StructLayout(LayoutKind.Auto)]
+    public partial struct ElementMoveParallelJob : IJobEntity
+    {
+        [ReadOnly] public float3 MovementDirection;
+
+        private void Execute(ref ElementComponent elementComponent, ref LocalTransform localTransform)
+        {
+            elementComponent.Position += MovementDirection;
+            localTransform.Position = elementComponent.Position;
+        }
+    }
+}
---
+++ Assets/MoveElements/Scripts/Systems/ElementMoveSystem.cs
@@ -0,0 +1,59 @@
+﻿using MoveElements.Scripts.Components;
+using MoveElements.Scripts.Data;
+using MoveElements.Scripts.Jobs;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+namespace MoveElements.Scripts.Systems
+{
+    [BurstCompile]
+    [UpdateInGroup(typeof(SimulationSystemGroup), OrderFirst = true)]
+    public partial struct ElementMoveSystem : ISystem
+    {
+        [BurstCompile]
+        public void OnCreate(ref SystemState state)
+        {
+            state.RequireForUpdate<ElementComponent>();
+        }
+        
+        [BurstCompile]
+        public void OnDestroy(ref SystemState state)
+        {
+        }
+        
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            var deltaTime = SystemAPI.Time.DeltaTime;
+            ScheduleJobs(ref state, deltaTime);
+        }
+
+        [BurstCompile]
+        private void ScheduleJobs(ref SystemState state, float deltaTime)
+        {
+            var elementMoveParallelJobHandle = ScheduleElementMoveJob(ref state, deltaTime);
+            state.Dependency = elementMoveParallelJobHandle;
+            elementMoveParallelJobHandle.Complete();
+        }
+        
+        [BurstCompile]
+        private JobHandle ScheduleElementMoveJob(ref SystemState state, float deltaTime)
+        {
+            var query = new EntityQueryBuilder(Allocator.Temp)
+                .WithAllRW<ElementComponent, LocalTransform>()
+                .Build(state.EntityManager);
+            
+            var movementDirection = new float3(1 * deltaTime, 0, 0);
+            var elementMoveParallelJobHandle = new ElementMoveParallelJob
+            {
+                MovementDirection = movementDirection
+            }.ScheduleParallel(query, state.Dependency);
+            
+            return elementMoveParallelJobHandle;
+        }
+    }
+}
