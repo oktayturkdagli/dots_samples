0cdff46a3eef5b1a9fcd6b46d3b69a1523f4eb65
Oktay Türkdağlı <oktayturkdagli@gmail.com>
2023-11-02 16:54:48 +0300

    feat(resources): add move osm resources

---
+++ Assets/MoveOsm/Scripts/Mono/ResourceLoader.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace MoveOsm.Scripts.Mono
+{
+    public class ResourceLoader : MonoBehaviour
+    {
+        private void Awake()
+        {
+            //Load Materials
+            Resources.LoadAsync("Prefabs/Line");
+            Resources.LoadAsync("Materials/Mat_Lanelet");
+            Resources.LoadAsync("Materials/Mat_Line_Arrow_Bidirectional_Dashed");
+            Resources.LoadAsync("Materials/Mat_Line_Arrow_Bidirectional_Solid");
+            Resources.LoadAsync("Materials/Mat_Line_Arrow_Common_Dashed");
+            Resources.LoadAsync("Materials/Mat_Line_Arrow_Common_Solid");
+
+            //Load Prefabs
+            Resources.Load("Prefabs/Line");
+        
+            //Load Mesh Assets
+            Resources.GetBuiltinResource<Mesh>("Sphere.fbx");
+            Resources.GetBuiltinResource<Mesh>("Plane.fbx");
+        }
+    }
+}
